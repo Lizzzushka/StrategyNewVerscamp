3d81b2a9e1ed38a249eb9abf7d666bc7ea95a2d4
Lizzzushka <lizushka.parkour@gmail.com>
2025-07-26 17:24:27 +0300

    17/24/ 26/07/2025 go to line ball and zero blue robot and also we angel to ball and mediana of 3 robots

--- bridge/strategy/strategy.py
+++ bridge/strategy/strategy.py
@@ -18,7 +18,9 @@ class Strategy:
         self,
     ) -> None:
         self.we_active = False
-        self.
+        self.idx = 2
+        self.puinum = 1
+
 
     def process(self, field: fld.Field) -> list[Optional[Action]]:
         """Game State Management"""
@@ -82,6 +84,46 @@ class Strategy:
         - actions[9] = Actions.BallGrab(0.0)
                 The robot number 9 grabs the ball at an angle of 0.0 (it looks to the right, along the OX axis)
         """
-        vecpovorot = (aux.Point(field.ball.get_pos().x+500))
-        vecpovorot2 = aux.rotate(vecpovorot, (3.14/4*2)+3.14 + time()/3)
-        actions[2] = Actions.GoToPoint(field.ball.get_pos() + vecpovorot2, 0)
+        """angell =  (field.ball.get_pos() - field.allies[self.idx].get_pos()).arg()
+        if self.puinum == 1:
+            actions[self.idx] = Actions.GoToPointIgnore(field.ball.get_pos(), angell)
+
+        disttball = aux.dist(field.ball.get_pos(), field.active_allies[self.idx].get_pos())
+        if disttball < 200:
+            self.puinum = 2
+        elif self.puinum == 2:
+            myGoalPoint = field.ally_goal
+            GoalPoint = field.ally_goal
+            mgp = aux.nearest_point_on_poly(field.allies[self.idx].get.pos(), myGoalPoint)
+            gp = aux.nearest_point_on_poly(field.allies[self.idx].get.pos(), GoalPoint)
+        if aux.dist(field.allies[self.idx].get_pos(), mgp) < aux.dist(field.allies[self.idx].get_pos(), gp):
+            mgp = gp
+        actions[self.idx] = Actions.GoToPointIgnore(mgp, angell)
+        distgoal = aux.dist(gp, field.allies[self.idx].get_pos())
+        if distgoal < 200:
+            self.puinum = 1
+        angleBlue = field.enemies[0].get_angle()   
+        angleYel = field.allies[0].get_angle()
+        angleYel4 = field.allies[4].get_angle()   
+        
+        vecBT = aux.Point(500, 0)
+        vecYT = aux.Point(500, 0)
+        vecYTT = aux.Point(500, 0)  
+        
+        vecY0 = aux.rotate(vecYT, angleYel)
+        vecB0 = aux.rotate(vecBT, angleBlue) 
+        vecY4 = aux.rotate(vecYTT, angleYel4) 
+
+        pointintr1 = aux.get_line_intersection(vecB0.unity() + field.enemies[0].get_pos(), vecB0.unity() * 2 + field.enemies[0].get.pos(), vecY0.unity() + field.allies[0].get.pos(), vecY0.unity() * 2 + field.allies[0].get_pos(), "LL" )
+        pointintr2 = aux.get_line_intersection(vecB0.unity() + field.enemies[0].get_pos(), vecY4.unity() * 2 + field.enemies[0].get.pos(), vecY4.unity()  + field.allies[4].get.pos(), vecY4.unity() * 2 + field.allies[4].get_pos(), "LL" )
+        pointintr3 = aux.get_line_intersection(vecY4.unity() + field.allies[4].get_pos(), vecY0.unity() * 2 + field.allies[4].get.pos(), vecY0.unity()  + field.allies[0].get.pos(), vecY0.unity() * 2 + field.allies[0].get_pos(), "LL" )
+    
+        if pointintr1 is None or pointintr2 is None or pointintr3 is None:
+            print("Нет пересечений")
+        else:
+            mediana = (pointintr1 + pointintr2 + pointintr3) / 3
+            print(mediana) """    
+
+        angell =  (field.ball.get_pos() - field.allies[self.idx].get_pos()).arg()
+        vecBallRob = aux.point_on_line(field.ball.get_pos(), field.enemies[0].get_pos(), -500)    
+        actions[self.idx] = Actions.GoToPointIgnore(vecBallRob, angell)     
